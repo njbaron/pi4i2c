e723468b24e31e95ab00600ff68dc2310d00d4de
Nicholas Baron <njbaron@users.noreply.github.com>
2018-01-07 00:01:19 -0700

    opps v2

--- src/main/java/com/devknight/pi4i2c/executable.java
+++ src/main/java/com/devknight/pi4i2c/executable.java
@@ -10,7 +10,7 @@ public class executable {
         System.out.println("Welcome to JavaI2C");
         try {
             mpu6050 mpu = new mpu6050((byte) 0x69);
-            I2CLcdDisplay lcd = new I2CLcdDisplay(2,16,1,1,1,1,1,1,1,1,1,1);
+            I2CLcdDisplay lcd = new I2CLcdDisplay(2,16,1,0x27,1,1,1,1,1,1,1,1);
             while (true) {
                 System.out.println("MPU Temp: " + mpu.get_temp());
                 double[] accelData = mpu.get_accel_data();
