68e1b50165e5f3f028da9ab8bb6c0f1430d1dad1
Nicholas Baron <njbaron@users.noreply.github.com>
2018-01-07 01:34:41 -0700

    omg

--- src/main/java/com/devknight/pi4i2c/executable.java
+++ src/main/java/com/devknight/pi4i2c/executable.java
@@ -6,8 +6,13 @@ import com.pi4j.component.lcd.impl.I2CLcdDisplay;
 
 public class executable {
 
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to JavaI2C");
+        long x;
+        long y;
+        long z;
         try {
             mpu6050 mpu = new mpu6050((byte) 0x69);
             I2CLcdDisplay lcd = new I2CLcdDisplay(2,16,1,0x27,3,0,1,2,7,6,5,4);
@@ -22,8 +27,13 @@ public class executable {
                 System.out.println("MPU Gyro x: " + gyroData[0]);
                 System.out.println("MPU Gyro y: " + gyroData[1]);
                 System.out.println("MPU Gyro z: " + gyroData[2]);
-                lcd.write(0,"x: " + Math.round(accelData[0]) + " y: " + Math.round(accelData[1]));
-                lcd.write(1,"z: " + Math.round(accelData[2]), 2);
+
+                x = Math.round(accelData[0]);
+                y = Math.round(accelData[1]);
+                z = Math.round(accelData[2]);
+
+                lcd.write(0,"x: " + String.format("%03d", x) + " y: " + String.format("%03d", y));
+                lcd.write(1,"z: " + String.format("%03d", z) , 2);
             }
         }
         catch(Exception e) {
